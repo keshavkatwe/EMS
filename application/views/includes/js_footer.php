352d5c353e74b12d0a9ce8c723e2cb6fd9939d3e
ishwarkatwe <ishwar.katwe0816@gmail.com>
2015-03-07 21:57:25 +0530

    toastr with custom js
    toastr short functions

--- application/views/includes/js_footer.php
+++ application/views/includes/js_footer.php
@@ -1,3 +1,17 @@
+<input type="hidden" id="base_url_hidden" value="<?php echo base_url(); ?>" />
+<?php   
+$success = $this->session->flashdata("show_success");
+$warning = $this->session->flashdata("show_warning");
+$error = $this->session->flashdata("show_error");
+$info = $this->session->flashdata("show_info");
+?>
+<input type="hidden" id="show_success_hidden" value="<?php echo (isset($success)) ? $success : ""; ?>" />
+<input type="hidden" id="show_error_hidden" value="<?php echo (isset($error)) ? $error : ""; ?>"  />
+<input type="hidden" id="show_warning_hidden" value="<?php echo (isset($warning)) ? $warning : ""; ?>"  />
+<input type="hidden" id="show_info_hidden" value="<?php echo (isset($info)) ? $info : ""; ?>"  />
+
+
+
 <!-- jQuery 2.1.3 -->
 <script src="<?php echo base_url('bower_components/jquery/jQuery-2.1.3.min.js') ?>"></script>
 <!-- Bootstrap 3.3.2 JS -->
@@ -8,6 +22,5 @@
 <script src="<?php echo base_url('bower_components/fastclick/fastclick.min.js') ?>"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url('dist/js/app.min.js') ?>" type="text/javascript"></script>
-
-
 <script src="<?php echo base_url('bower_components/toastr/toastr.min.js') ?>"></script>
+<script src="<?php echo base_url('dist/js/custom_js.js') ?>"></script>
