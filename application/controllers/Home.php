a42107a1fe1195ec1ba3d4f788fe4bbd38785651
Keshav Katwe <keshav.katwe@gmail.com>
2015-03-07 20:06:31 +0530

    Start of the project

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Home
+ *
+ * @author Keshav K
+ */
+class Home extends Custom_controller{
+    
+    public function index() {
+        
+    }
+    
+}
