8a9c586438b848ce897a71491a0176ee3644d080
ishwarkatwe <ishwar.katwe0816@gmail.com>
2015-03-08 17:41:26 +0530

    home page demo look

--- application/views/home.php
+++ application/views/home.php
@@ -6,8 +6,8 @@
 
         <?php $this->load->view('includes/css_header') ?>
     </head>
-    
-    <body class="<?php echo THEME_COLOR;?>">
+
+    <body class="<?php echo THEME_COLOR; ?>">
         <!-- Site wrapper -->
         <div class="wrapper">
             <?php $this->load->view('includes/top_menu') ?>
@@ -16,30 +16,45 @@
             <!-- Right side column. Contains the navbar and content of the page -->
             <div class="content-wrapper">
                 <!-- Content Header (Page header) -->
-                <section class="content-header">
-                    <h1>
-                        Home
-                        <small>it all starts here</small>
-                    </h1>
-                </section>
+              
 
                 <!-- Main content -->
                 <section class="content">
 
-                    <!-- Default box -->
-                    <div class="box">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Title</h3>
-                        </div>
-                        <div class="box-body">
-                            Start creating your amazing application!
-                            
-                            
-                        </div><!-- /.box-body -->
-                        <div class="box-footer">
-                            Footer
-                        </div><!-- /.box-footer-->
-                    </div><!-- /.box -->
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-aqua"><i class="ion ion-ios-person-outline"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Students</span>
+                                    <span class="info-box-number">1,000</span>
+                                </div><!-- /.info-box-content -->
+                            </div><!-- /.info-box -->
+                        </div><!-- /.col -->
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-red"><i class="ion ion-ios-people-outline"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Faculty</span>
+                                    <span class="info-box-number">410</span>
+                                </div><!-- /.info-box-content -->
+                            </div><!-- /.info-box -->
+                        </div><!-- /.col -->
+
+                        <!-- fix for small devices only -->
+                        <div class="clearfix visible-sm-block"></div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-green"><i class="ion ion-ios-book-outline"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Subjects</span>
+                                    <span class="info-box-number">760</span>
+                                </div><!-- /.info-box-content -->
+                            </div><!-- /.info-box -->
+                        </div><!-- /.col -->
+                       
+                    </div>
 
                 </section><!-- /.content -->
             </div><!-- /.content-wrapper -->
@@ -49,5 +64,5 @@
 
         <?php $this->load->view('includes/js_footer') ?>
     </body>
-    
+
 </html>
